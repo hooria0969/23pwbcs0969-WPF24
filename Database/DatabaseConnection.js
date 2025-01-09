8bee3e9819b6b29bb200a62cbd66d531bd636411
hooria0969 <hooriak466@gmail.com>
2025-01-09 13:35:06 +0500

    updated

--- Database/DatabaseConnection.js
+++ Database/DatabaseConnection.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config(); // Load environment variables from a .env file (for local development)
+
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/Assignment02-DB`, {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
       });
-      console.log(`MongoDB Connected: {conn.connection.host}`);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-      console.error(error.message);
+      console.error(`Error: ${error.message}`);
+
       process.exit(1);
     }
   }
